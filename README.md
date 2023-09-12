1e06a32fc571c446a33e76c38a1e3ccdad3442b5
breyr <breyr2021@gmail.com>
2023-09-11 20:17:07 -0400

    Update script.py and README

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Python script to launch a website to Cloudflare in one file.
 
 ## How to run:
 1. Clone this repo
-2. Modify `REPONAME` to the worker repository you want to clone
+2. Modify `REPONAME`, `WORKER_NAME`, `GITHUB_ACCT`
 3. Run `script.py`
--- script.py
+++ script.py
@@ -8,9 +8,11 @@ Prereqs:
 
 import subprocess
 
-REPONAME = "comments-api-shell"
+REPO_NAME = "example-repo"
+WORKER_NAME = "example-worker"
+GITHUB_ACCT = "testname"
 
-commands = [f"wget https://github.com/breyr/{REPONAME}/archive/main.zip",
+commands = [f"wget https://github.com/{GITHUB_ACCT}/{REPO_NAME}/archive/main.zip",
             "unzip main.zip",
             "rm main.zip",
             # install dependencies if you want to modify code
@@ -19,10 +21,9 @@ commands = [f"wget https://github.com/breyr/{REPONAME}/archive/main.zip",
             "npm list -g | grep wrangler || npm install -g wrangler",
             # check to see if user is logged in, if not have them log in
             "wrangler whoami || wrangler login",
-            f"wrangler d1 create {REPONAME}-db",
-            f"wrangler d1 execute comments-api-shell-db --file {REPONAME}-main/schemas/schema.sql",
+            f"wrangler d1 create {REPO_NAME}-db",
             # deploy to Cloudflare
-            f"cd {REPONAME}-main && wrangler deploy"]
+            f"cd {REPO_NAME}-main && wrangler deploy"]
 
 for command in commands:
     print(f"Running command: {command}")
@@ -30,9 +31,11 @@ for command in commands:
         output = subprocess.check_output(command, shell=True, text=True)
         # if wrangler d1 create dev is run, append the output to wrangler.toml
         # only need last 5 lines of output
-        if command == f"wrangler d1 create {REPONAME}-db":
-            with open(f"{REPONAME}/wrangler.toml", "a") as f:
+        if command == f"wrangler d1 create {REPO_NAME}-db":
+            with open(f"{REPO_NAME}/wrangler.toml", "a") as f:
                 # write new line to separate configs in toml file
+                f.write(
+                    f"name = \"{WORKER_NAME}\"\nmain = \"index.js\"\ncompatibility_date = \"2023-09-04\"")
                 f.write("\n")
                 for line in output.split("\n")[-5:]:
                     f.write(line + "\n")
